1433482ffb3ff5b64f5d9a7663627b5927b48e21
folarin <folami25@gmail.com>
2024-02-17 11:46:40 +0100

    modified the program by adding functions: DRY

--- profitCalculator.go
+++ profitCalculator.go
@@ -4,29 +4,42 @@ import (
 	"fmt"
 )
 
+const taxRate float64 = 19
+
 func main() {
-	const taxRate float64 = 19
-	var quantitySold, unitPrice, grossRevenue, corporateTax, netRevenue float64
+	var quantitySold, unitPrice, grr, corporateTax, netRevenue float64
 
 	// get values for quantity and price
-	fmt.Print("Enter the number of items sold: ")
+	textRequest("Enter the number of items sold: ")
 	fmt.Scan(&quantitySold)
-	fmt.Print("Enter the unit price/item: ")
+
+	textRequest("Enter the unit price/item: ")
 	fmt.Scan(&unitPrice)
 
-	// calculate gross revenue
-	grossRevenue = quantitySold * unitPrice
-	fmt.Printf("Your revenue before tax is %.2f £\n", grossRevenue)
+	// calculate gross revenue, corportate tax, net revenue and profit ratio
+	// if a function returns multiple values, like this one, you can store it in those variables
+	// grossRevenue = quantitySold * unitPrice
+	// corporateTax = grossRevenue * (taxRate / 100)
+	// netRevenue = grossRevenue - corporateTax
+	// ratio = grossRevenue / netRevenue
+	revNums(quantitySold, unitPrice)
+	grr, corporateTax, netRevenue, ratio := revNums(quantitySold, unitPrice)
 
-	// calculate corporate tax
-	corporateTax = grossRevenue * (taxRate / 100)
+	fmt.Printf("Your revenue before tax is %.2f £\n", grr)
 	fmt.Printf("Your corporate tax stands at %.2f £\n", corporateTax)
-
-	// calculate revenue after tax
-	netRevenue = grossRevenue - corporateTax
 	fmt.Printf("Your actual revenue is %.2f £\n", netRevenue)
-
-	// calculate ratio of gross revenue to net revenue
-	var ratio = grossRevenue / netRevenue
 	fmt.Printf("The ratio of gross revenue to net revenue is %.2f \n", ratio)
+}
+
+// now to write a custom function that'll replace fmt used to print text
+func revNums(quantitySold, unitPrice float64) (float64, float64, float64, float64) {
+	grr := quantitySold * unitPrice
+	corporateTax := grr * (taxRate / 100)
+	netRevenue := grr - corporateTax
+	ratio := grr / netRevenue
+	return grr, corporateTax, netRevenue, ratio
+}
+
+func textRequest(text string) {
+	fmt.Println(text)
 }
